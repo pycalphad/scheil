26d94ff292e876da87552154d7f51670a0fffd1d
bocklund <brandonbocklund@gmail.com>
2018-09-26 09:50:05 -0400

    Document SolidificationResult

--- scheil/solidification_result.py
+++ scheil/solidification_result.py
@@ -1,6 +1,30 @@
 import numpy as np
 
 class SolidifcationResult():
+    """Short summary.
+
+    Parameters
+    ----------
+    temperatures : list
+        List of simulation temperatures.
+    x_liquid : list
+        Mole fraction of the liquid phase at each temperature.
+    fraction_solid : list
+        Fraction of liquid at each temperature.
+    phase_amounts : dict
+        Dictionary of {phase_name: amount_list} where amount_list is a list of
+        cumulative phase amounts at each temperature.
+
+    Attributes
+    ----------
+    fraction_liquid : list
+        Description of attribute `fraction_liquid`.
+    x_liquid
+    fraction_solid
+    temperatures
+    phase_amounts
+
+    """
     def __init__(self, x_liquid, fraction_solid, temperatures, phase_amounts):
         self.x_liquid = x_liquid
         self.fraction_solid = fraction_solid
