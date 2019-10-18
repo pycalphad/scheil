8e400a13016e3f95ddca5d7443cbab762713c7ba
bocklund <brandonbocklund@gmail.com>
2019-10-18 11:28:34 -0400

    ENH: Adaptive sampling and order-disorder splitting working

--- scheil/simulate.py
+++ scheil/simulate.py
@@ -1,16 +1,19 @@
-
-from pycalphad import equilibrium, variables as v
-from pycalphad.core.utils import instantiate_models
-from pycalphad.codegen.callables import build_callables
-from scheil import SolidifcationResult
 import numpy as np
 from collections import defaultdict
+from pycalphad import equilibrium, variables as v
+from pycalphad.codegen.callables import build_callables
+from pycalphad.core.utils import instantiate_models, generate_dof, \
+    unpack_components
+from .solidification_result import SolidifcationResult
+from .utils import order_disorder_dict, local_sample, order_disorder_eq_phases
 
-NAN_DICT = defaultdict(lambda:np.nan)
+NAN_DICT = defaultdict(lambda: np.nan)
+
 
 def simulate_scheil_solidification(dbf, comps, phases, composition,
                                    start_temperature, step_temperature=1.0,
-                                   liquid_phase_name='LIQUID', stop=0.0001, verbose=False):
+                                   liquid_phase_name='LIQUID', eq_kwargs=None,
+                                   stop=0.0001, verbose=False, adaptive=True):
     """Perform a Scheil-Gulliver solidification simulation.
 
     Parameters
@@ -25,50 +28,81 @@ def simulate_scheil_solidification(dbf, comps, phases, composition,
         Dictionary of independent `v.X` composition variables.
     start_temperature : float
         Starting temperature for simulation. Must be single phase liquid.
-    step_temperature : float, optional
+    step_temperature : Optional[float]
         Temperature step size. Defaults to 1.0.
-    liquid_phase_name : str
+    liquid_phase_name : Optional[str]
         Name of the phase treated as liquid.
+    eq_kwargs: Optional[Dict[str, Any]]
+        Keyword arguments for equilibrium
+    stop: Optional[float]
+        Stop when the phase fraction of liquid is below this amount.
+    adaptive: Optional[bool]
+        Whether
 
     Returns
     -------
-    scheil.solidification_result.SolidifcationResult
+    SolidifcationResult
 
     """
-    STEP_SCALE_FACTOR = 1.1  # How much to try to adapt the temperature step by
-    MAXIMUM_STEP_SIZE_REDUCTION = 10.0
+    eq_kwargs = eq_kwargs or dict()
+    STEP_SCALE_FACTOR = 1.2  # How much to try to adapt the temperature step by
+    MAXIMUM_STEP_SIZE_REDUCTION = 5.0
     T_STEP_ORIG = step_temperature
     models = instantiate_models(dbf, comps, phases)
+    if verbose:
+        print('building callables... ', end='')
     cbs = build_callables(dbf, comps, phases, models, additional_statevars={v.P, v.T, v.N}, build_gradients=True, build_hessians=True)
-    solid_phases = sorted(set(phases)-{'GAS', liquid_phase_name})
+    if verbose:
+        print('done')
+    solid_phases = sorted(set(phases) - {'GAS', liquid_phase_name})
     temp = start_temperature
     independent_comps = sorted(composition.keys(), key=str)
     x_liquid = [composition]
     fraction_solid = [0.0]
     temperatures = [temp]
     phase_amounts = {ph: [0.0] for ph in solid_phases}
+    ord_disord_dict = order_disorder_dict(dbf, comps, phases)
+
+    if adaptive and ('points' in eq_kwargs.get('calc_opts', {})):
+        # Dynamically add points as the simulation runs
+        species = unpack_components(dbf, comps)
+        dof_dict = {ph: generate_dof(dbf.phases[ph], species)[1] for ph in phases}
+    else:
+        adaptive = False
 
     phases_seen = {liquid_phase_name, ''}
     while fraction_solid[-1] < 1:
-        conds = {v.T: temp, v.P: 101325}
+        conds = {v.T: temp, v.P: 101325.0, v.N: 1.0}
         comp_conds = x_liquid[-1]
         fmt_comp_conds = ', '.join(['{}={:0.2f}'.format(c, val) for c, val in comp_conds.items()])
         conds.update(comp_conds)
-        eq = equilibrium(dbf, comps, phases, conds, callables=cbs, model=models)
-        eq_phases = eq["Phase"].values.squeeze()
+        eq = equilibrium(dbf, comps, phases, conds, callables=cbs, model=models, **eq_kwargs)
+        if adaptive:
+            points_dict = eq_kwargs['calc_opts']['points']
+            for vtx in range(eq.vertex.size):
+                masked = eq.isel(vertex=vtx)
+                ph = str(masked.Phase.values.squeeze())
+                pts = points_dict.get(ph)
+                if pts is not None:
+                    if verbose:
+                        print(f'Adding points to {ph}.', end=' ')
+                    dof = dof_dict[ph]
+                    points_dict[ph] = np.concatenate([pts, local_sample(masked.Y.values.squeeze()[:sum(dof)].reshape(1, -1), dof, pdens=20)], axis=0)
+
+        eq_phases = order_disorder_eq_phases(eq, ord_disord_dict)
         num_eq_phases = np.nansum(eq_phases != '')
         new_phases_seen = set(eq_phases).difference(phases_seen)
         if len(new_phases_seen) > 0:
             if verbose:
-                print('New phases seen: {}. Resetting step size'.format(new_phases_seen))
+                print('New phases seen: {}.'.format(new_phases_seen), end=' ')
             phases_seen |= new_phases_seen
-            temp += step_temperature
-            step_temperature = T_STEP_ORIG
-            continue
+            # temp += step_temperature
+            # step_temperature = T_STEP_ORIG
+            # continue
         if liquid_phase_name not in eq["Phase"].values.squeeze():
             if num_eq_phases == 0:
-                print('Convergence failure: T={} and {}'.format(temp, fmt_comp_conds))
-            if T_STEP_ORIG/step_temperature > MAXIMUM_STEP_SIZE_REDUCTION:
+                print('Convergence failure: T={} and {}'.format(temp, fmt_comp_conds), end=' ')
+            if T_STEP_ORIG / step_temperature > MAXIMUM_STEP_SIZE_REDUCTION:
                 if verbose:
                     print('No liquid phase found at T={}, {} (Found {}). Maximum step size reduction exceeded. Stopping.'.format(temp, fmt_comp_conds, eq_phases))
                 break
@@ -86,28 +120,29 @@ def simulate_scheil_solidification(dbf, comps, phases, composition,
         current_fraction_solid = float(fraction_solid[-1])
         found_phase_amounts = [(liquid_phase_name, np_liq)]  # tuples of phase name, amount
         for solid_phase in solid_phases:
-            if solid_phase not in eq["Phase"].values.squeeze():
+            if solid_phase not in eq_phases:
                 phase_amounts[solid_phase].append(0.0)
                 continue
-            np_tieline = np.nansum(eq.where(eq["Phase"] == solid_phase)["NP"].values.squeeze())
+            np_tieline = np.nansum(eq.isel(vertex=eq_phases.index(solid_phase))["NP"].values.squeeze())
             found_phase_amounts.append((solid_phase, np_tieline))
-            delta_fraction_solid = (1-current_fraction_solid) * np_tieline
+            delta_fraction_solid = (1 - current_fraction_solid) * np_tieline
             current_fraction_solid += delta_fraction_solid
             phase_amounts[solid_phase].append(delta_fraction_solid)
         fraction_solid.append(current_fraction_solid)
         temperatures.append(temp)
-        NL = 1-fraction_solid[-1]
+        NL = 1 - fraction_solid[-1]
         if verbose:
             phase_amnts = ' '.join(['NP({})={:0.3f}'.format(ph, amnt) for ph, amnt in found_phase_amounts])
             if NL < 1.0e-3:
-                print('T={:0.3f}, {}, ΔT={:0.3f}, NL: {:.2E}, {}'.format(temp, fmt_comp_conds, step_temperature, NL, phase_amnts))
+                print('T={:0.3f}, {}, ΔT={:0.3f}, NL: {:.2E}, {}'.format(temp, fmt_comp_conds, step_temperature, NL, phase_amnts), end=' ')
             else:
-                print('T={:0.3f}, {}, ΔT={:0.3f}, NL: {:0.3f}, {}'.format(temp, fmt_comp_conds, step_temperature, NL, phase_amnts))
+                print('T={:0.3f}, {}, ΔT={:0.3f}, NL: {:0.3f}, {}'.format(temp, fmt_comp_conds, step_temperature, NL, phase_amnts), end=' ')
         if NL < stop:
             if verbose:
                 print('Liquid fraction below criterion {} . Stopping at fmt_comp_conds'.format(stop))
             break
-
+        if verbose:
+            print()  # add line break
         temp -= step_temperature
 
     if fraction_solid[-1] < 1:
@@ -117,8 +152,11 @@ def simulate_scheil_solidification(dbf, comps, phases, composition,
         # set the final phase amount to the phase fractions in the eutectic
         # this method gives the sum total phase amounts of 1.0 by construction
         for solid_phase in solid_phases:
-            amount = np.nansum(eq["NP"].isel(T=0,P=0).where(eq["Phase"] == solid_phase).values)
-            phase_amounts[solid_phase].append(float(amount)*(1-current_fraction_solid))
+            if solid_phase in eq_phases:
+                amount = np.nansum(eq.isel(vertex=eq_phases.index(solid_phase))["NP"].values.squeeze())
+                phase_amounts[solid_phase].append(float(amount) * (1 - current_fraction_solid))
+            else:
+                phase_amounts[solid_phase].append(0.0)
 
     # take the instantaneous phase amounts and make them cumulative
     phase_amounts = {ph: np.cumsum(amnts).tolist() for ph, amnts in phase_amounts.items()}
@@ -128,14 +166,19 @@ def simulate_scheil_solidification(dbf, comps, phases, composition,
 
 def simulate_equilibrium_solidification(dbf, comps, phases, composition,
                                         start_temperature, end_temperature, step_temperature,
-                                        liquid_phase_name='LIQUID', callables=None):
+                                        liquid_phase_name='LIQUID', verbose=False):
     # Compute the equilibrium solidification path
-    solid_phases = sorted(set(phases)-{'GAS', liquid_phase_name})
+    solid_phases = sorted(set(phases) - {'GAS', liquid_phase_name})
     independent_comps = sorted(composition.keys(), key=str)
-    callables = build_callables(dbf, comps, phases)
+    models = instantiate_models(dbf, comps, phases)
+    if verbose:
+        print('building callables... ', end='')
+    cbs = build_callables(dbf, comps, phases, models, additional_statevars={v.P, v.T, v.N}, build_gradients=True, build_hessians=True)
+    if verbose:
+        print('done')
     conds = {v.T: (end_temperature, start_temperature, step_temperature), v.P: 101325}
     conds.update(composition)
-    eq = equilibrium(dbf, comps, phases, conds, **callables)
+    eq = equilibrium(dbf, comps, phases, conds, callables=cbs)
 
     temperatures = eq["T"].values.tolist()
     x_liquid = []
---
+++ scheil/utils.py
@@ -0,0 +1,167 @@
+import itertools
+from collections import defaultdict
+import numpy as np
+from scipy.stats import norm
+from pycalphad.core.utils import unpack_components, generate_dof
+
+
+def is_ordered(site_fracs, subl_dof, symmetric_subl_idx, **kwargs):
+    """Return True if the site fraction configuration is ordered
+
+    Parameters
+    ----------
+    site_fracs : numpy.ndarray[float,N]
+        Site fraction array for a phase of length sum(subl_dof) (can be padded
+        with arbitrary data, as returned by equilibrium calculations).
+    subl_dof : list[int]
+        List of the number of components active in each sublattice. Size should
+        be equivalent to the number of sublattices in the phase.
+    symmetric_subl_idx : list[list[int]]
+        List of sets of symmetrically equivalent sublattice indexes (as a list).
+        If sublattices at index 0 and index 1 are symmetric, as index 2 and
+        index 3, then the symmetric_subl_idx will be [[0, 1], [2, 3]].
+    kwargs :
+        Additional keyword arguments passed to ``np.isclose`` to check if site
+        fractions in symmetric sublattices are distinct.
+
+    Returns
+    -------
+    bool
+
+    Examples
+    --------
+    >>> dbf = Database('Fe-Ni-Ti.tdb')
+    >>> subl_dof = [4, 4, 1] # sublattice model is (Fe,Ni,Ti,Va)(Fe,Ni,Ti,Va)(Va)
+    >>> symm = [[0, 1]]  # sublattices 0 and 1 should be equivalent
+    >>> res = equilibrium(dbf, ['FE', 'NI', 'TI', 'VA'], ['BCC2'], {v.P: 101325, v.T: 1200, v.N: 1, v.X('FE'): 0.001, v.X('NI'): 0.001})
+    >>> is_ordered(res.Y.isel(vertex=0).values.squeeze(), subl_dof, symm)
+    False
+    >>> res = equilibrium(dbf, ['FE', 'NI', 'TI', 'VA'], ['BCC2'], {v.P: 101325, v.T: 1200, v.N: 1, v.X('FE'): 0.25, v.X('NI'): 0.25})
+    >>> is_ordered(res.Y.isel(vertex=0).values.squeeze(), subl_dof, symm)
+    True
+
+    """
+    # For each sublattice, create a ``slice`` object for slicing the site
+    # fractions of that particular sublattice from the site fraction array
+    subl_slices = []
+    for subl_idx in range(len(subl_dof)):
+        start_idx = np.sum(subl_dof[:subl_idx], dtype=np.int)
+        end_idx = start_idx + subl_dof[subl_idx]
+        subl_slices.append(slice(start_idx, end_idx))
+
+    # For each set of symmetrically equivalent sublattices
+    for symm_subl in symmetric_subl_idx:
+        # Check whether the site fractions of each pair of symmetrically
+        # equivalent sublattices are ordered or disordered
+        for idx1, idx2 in itertools.combinations(symm_subl, 2):
+            # A phase is ordered if any pair of sublattices does not have
+            # equal (within numerical tolerance) site fractions
+            pair_is_ordered = np.any(~np.isclose(site_fracs[subl_slices[idx1]], site_fracs[subl_slices[idx2]], **kwargs))
+            if pair_is_ordered:
+                return True
+    return False
+
+
+def order_disorder_dict(dbf, comps, phases):
+    """Return a dictionary with the sublattice degrees of freedom and equivalent
+    sublattices for order/disorder phases
+
+    Parameters
+    ----------
+    dbf : pycalphad.Database
+    comps : list[str]
+        List of active components to consider
+    phases : list[str]
+        List of active phases to consider
+
+    Returns
+    -------
+    dict
+
+    Notes
+    -----
+    Phases which should be checked for ordered/disordered configurations are
+    determined heuristically for this script.
+
+    The heuristic for a phase satisfies the following:
+    1. The phase is the ordered part of an order-disorder model
+    2. The equivalent sublattices have all the same number of elements
+    """
+    species = unpack_components(dbf, comps)
+    ord_disord_phases = {}
+    for phase_name in phases:
+        phase_obj = dbf.phases[phase_name]
+        if phase_name == phase_obj.model_hints.get('ordered_phase', ''):
+            # This phase is active and modeled with an order/disorder model.
+            dof = generate_dof(dbf.phases[phase_name], species)[1]
+            # Define the symmetrically equivalent sublattices as any sublattices
+            # that have the same site ratio. Create a {site_ratio: [subl idx]} dict
+            site_ratio_idxs = defaultdict(lambda: [])
+            for subl_idx, site_ratio in enumerate(phase_obj.sublattices):
+                site_ratio_idxs[site_ratio].append(subl_idx)
+            equiv_sublattices = list(site_ratio_idxs.values())
+            ord_disord_phases[phase_name] = {
+                'subl_dof': dof,
+                'symmetric_subl_idx': equiv_sublattices,
+                'disordered_phase': phase_obj.model_hints['disordered_phase']
+            }
+    return ord_disord_phases
+
+
+def order_disorder_eq_phases(eq_result, order_disorder_dict):
+    """Return a list corresponding to the eq_result.Phase with order/disorder
+    phases named correctly.
+    """
+    eq_phases = []
+    for vtx in eq_result.vertex.values:
+        eq_phase = str(eq_result["Phase"].isel(vertex=vtx).values.squeeze())
+        site_fracs = eq_result["Y"].isel(vertex=vtx).values.squeeze()
+        if eq_phase in order_disorder_dict:
+            odd = order_disorder_dict[eq_phase]
+            is_ord = is_ordered(site_fracs, odd['subl_dof'], odd['symmetric_subl_idx'])
+            if is_ord:
+                eq_phases.append(eq_phase)
+            else:
+                eq_phases.append(odd['disordered_phase'])
+        else:
+            eq_phases.append(eq_phase)
+    return eq_phases
+
+
+def local_sample(sitefracs, comp_count, pdens=100, stddev=0.05):
+    """Sample from a normal distribution around the optimal site fractions
+
+
+    Parameters
+    ----------
+    sitefracs : np.ndarray[:, :]
+        2d array of site fractions of shape (N, len(dof))
+    comp_count : Sequence[int]
+        Number of active components in each sublattice, e.g.
+        (FE,NI,TI)(FE,NI)(FE,NI,TI) is [3, 2, 3]
+    pdens : Optional[int]
+        Number of points to add locally
+    stddev : Optional[float]
+        Standard deviation for the normal distribution to sample from
+
+    Returns
+    -------
+    np.ndarray[:, :]
+        Shape (pdens, len(dof))
+
+    """
+    # Need to take the absolute value, in order to make all the candidate site fractions positive
+    # There's probably a better distribution, but this works. Vacancies may cause trouble with this kind of normal distribution, since they are often near zero.
+    # Maybe like a gamma between Y=0 and Y=1, with a mean (or cdf of 0.5) optimal site fraction
+    pts = np.concatenate(np.abs(norm(sitefracs, stddev).rvs([pdens, sitefracs.shape[0], sitefracs.shape[-1]])), axis=0)
+    # add on the original points, since these are the actual minima
+    pts = np.concatenate([pts, sitefracs], axis=0)
+    # Need to normalize in each sublattice
+    # comp_count is # of elements per sublattice, e.g.
+    cur_idx = 0
+    for ctx in comp_count:
+        end_idx = cur_idx + ctx
+        pts[:, cur_idx:end_idx] /= pts[:, cur_idx:end_idx].sum(axis=1)[:, None]
+        cur_idx = end_idx
+    return pts
+
