pycalphad/scheil

commit 5f8d4c57121a9f333d0cab560870db52cf18b829

FIX: Call filter_phases to remove phases that cannot exist (#11)

scheil/simulate.py
@@ -3,7 +3,7 @@ import numpy as np
 from pycalphad import equilibrium, variables as v
 from pycalphad.codegen.callables import build_callables
 from pycalphad.core.utils import instantiate_models, generate_dof, \
-    unpack_components
+    unpack_components, filter_phases
 from .solidification_result import SolidificationResult
 from .utils import order_disorder_dict, local_sample, order_disorder_eq_phases, get_phase_amounts
 
@@ -85,6 +85,7 @@ def simulate_scheil_solidification(dbf, comps, phases, composition,
     STEP_SCALE_FACTOR = 1.2  # How much to try to adapt the temperature step by
     MAXIMUM_STEP_SIZE_REDUCTION = 5.0
     T_STEP_ORIG = step_temperature
+    phases = filter_phases(dbf, unpack_components(dbf, comps), phases)
     models = instantiate_models(dbf, comps, phases)
     if verbose:
         print('building callables... ', end='')
@@ -245,6 +246,7 @@ def simulate_equilibrium_solidification(dbf, comps, phases, composition,
 
     """
     eq_kwargs = eq_kwargs or dict()
+    phases = filter_phases(dbf, unpack_components(dbf, comps), phases)
     ord_disord_dict = order_disorder_dict(dbf, comps, phases)
     solid_phases = sorted(set(phases) - {'GAS', liquid_phase_name})
     independent_comps = sorted([str(comp)[2:] for comp in composition.keys()])

commit 9dd866fd303c883014f85afdf53f86363275a4c5

FIX: Fix removing disordered phase candidates (#16)
Fixes a regression introduced in #11, where `filter_phases` was introduced to remove phases that cannot be stable or phases where the disordered part is given as a candidate phase. The change in #11 made it so the disordered phase was not a part of the list of `solid_phases` that could form during the solidification simulations. With the disordered phase name not included in the `solid_phases`, the `order_disorder_eq_phases` function that renamed the ordered phases with disordered configurations to their disordered counterpart would lead to the phase amount of disordered configurations to _not_ be saved.

This PR fixes that and adds a parameterized test case that tests that the disordered phase always is accounted for regardless of whether or not the ordered phase is in the candidate phases or not.

scheil/simulate.py
@@ -87,20 +87,21 @@ def simulate_scheil_solidification(dbf, comps, phases, composition,
     MAXIMUM_STEP_SIZE_REDUCTION = 5.0
     T_STEP_ORIG = step_temperature
     phases = filter_phases(dbf, unpack_components(dbf, comps), phases)
+    ord_disord_dict = order_disorder_dict(dbf, comps, phases)
     models = instantiate_models(dbf, comps, phases)
     if verbose:
         print('building callables... ', end='')
     cbs = build_callables(dbf, comps, phases, models, additional_statevars={v.P, v.T, v.N}, build_gradients=True, build_hessians=True)
     if verbose:
         print('done')
-    solid_phases = sorted(set(phases) - {liquid_phase_name})
+    filtered_disordered_phases = {ord_ph_dict['disordered_phase'] for ord_ph_dict in ord_disord_dict.values()}
+    solid_phases = sorted((set(phases) | filtered_disordered_phases) - {liquid_phase_name})
     temp = start_temperature
     independent_comps = sorted([str(comp)[2:] for comp in composition.keys()])
     x_liquid = {comp: [composition[v.X(comp)]] for comp in independent_comps}
     fraction_solid = [0.0]
     temperatures = [temp]
     phase_amounts = {ph: [0.0] for ph in solid_phases}
-    ord_disord_dict = order_disorder_dict(dbf, comps, phases)
 
     if adaptive and ('points' in eq_kwargs.get('calc_opts', {})):
         # Dynamically add points as the simulation runs
@@ -250,7 +251,8 @@ def simulate_equilibrium_solidification(dbf, comps, phases, composition,
     eq_kwargs = eq_kwargs or dict()
     phases = filter_phases(dbf, unpack_components(dbf, comps), phases)
     ord_disord_dict = order_disorder_dict(dbf, comps, phases)
-    solid_phases = sorted(set(phases) - {liquid_phase_name})
+    filtered_disordered_phases = {ord_ph_dict['disordered_phase'] for ord_ph_dict in ord_disord_dict.values()}
+    solid_phases = sorted((set(phases) | filtered_disordered_phases) - {liquid_phase_name})
     independent_comps = sorted([str(comp)[2:] for comp in composition.keys()])
     models = instantiate_models(dbf, comps, phases)
     if verbose:

scheil/utils.py
@@ -69,7 +69,7 @@ def is_ordered(site_fracs, subl_dof, symmetric_subl_idx, **kwargs):
     # fractions of that particular sublattice from the site fraction array
     subl_slices = []
     for subl_idx in range(len(subl_dof)):
-        start_idx = np.sum(subl_dof[:subl_idx], dtype=np.int)
+        start_idx = np.sum(subl_dof[:subl_idx], dtype=np.int_)
         end_idx = start_idx + subl_dof[subl_idx]
         subl_slices.append(slice(start_idx, end_idx))
 
@@ -197,4 +197,3 @@ def local_sample(sitefracs, comp_count, pdens=100, stddev=0.05):
         pts[:, cur_idx:end_idx] /= pts[:, cur_idx:end_idx].sum(axis=1)[:, None]
         cur_idx = end_idx
     return pts
-

tests/test_chen2009.py
@@ -4,6 +4,7 @@ import os
 import numpy as np
 from pycalphad import Database, variables as v
 from scheil import simulate_scheil_solidification
+import pytest
 
 DB_CHEN = Database(os.path.join(os.path.dirname(__file__), 'sl_chen.tdb'))
 
@@ -15,6 +16,60 @@ def test_binary_A_B():
     step = 5.0
 
     sol_res = simulate_scheil_solidification(DB_CHEN, ['A', 'B'], ['ALPHA', 'BETA', 'LIQUID'], comp, start, step_temperature=step, stop=1e-8)
+    print(f"Converged to stopping criteria: {sol_res.converged}")
+
+    phase_amnts = sol_res.phase_amounts
+    # Check that the first solid phase to form is ALPHA and occurs at ~768 K
+    idx_first_solid = np.nonzero(np.array(sol_res.fraction_solid) > 0)[0][0]
+    assert phase_amnts['ALPHA'][idx_first_solid] > 0
+    assert np.isclose(phase_amnts['BETA'][idx_first_solid], 0)
+    # increase step atol a little for it to be inclusive
+    assert np.isclose(sol_res.temperatures[idx_first_solid], 768, atol=step * 1.1)
+
+    # Check that the last solid phase to form is BETA only and terminates at 600 K
+    assert np.isclose(phase_amnts['ALPHA'][-1], 0)
+    assert phase_amnts['BETA'][-1] > 0
+    if sol_res.converged:
+        # May have converged prior to the eutectic, but not below
+        assert sol_res.temperatures[-1] < 600 + step * 2 and sol_res.temperatures[-1] > 600
+    else:
+        # increase step atol a a litte, since it should have ended at the eutectic
+        assert np.isclose(sol_res.temperatures[-1], 600, atol=step * 1.1)
+
+    # Check that the temperature where the ALPHA to BETA switch occurs is close to the peritectic temperature of 647 K.
+    idx_first_beta = np.nonzero(np.array(phase_amnts['BETA']) > 0)[0][0]
+    idx_last_alpha = np.nonzero(np.array(phase_amnts['ALPHA']) > 0)[0][-1]
+    assert (idx_last_alpha + 1) == idx_first_beta
+    # increase step atol a little for it to be inclusive
+    assert np.isclose(sol_res.temperatures[idx_first_beta], 647, atol=step * 1.1)
+
+@pytest.mark.parametrize(
+    'phases',
+    (
+        ['ALPHA', 'BETA', 'ORD_BETA', 'LIQUID'],
+        ['ALPHA', 'ORD_BETA', 'LIQUID'],
+        ['ALPHA', 'BETA', 'LIQUID'],
+    )
+)
+def test_binary_A_B_with_ordering(phases):
+    """Tests for the Scheil properties of the A-B binary peritectic system with an ordered phase ORD_BETA"""
+    with open(os.path.join(os.path.dirname(__file__), 'sl_chen.tdb')) as fp:
+        tdb_str = fp.read()
+    # Append an ordered phase, 'ORD_BETA' with no ordering parameters
+    ordered_phase_str = """
+    TYPE_DEFINITION L GES A_P_D  ORD_BETA DIS_PART BETA   ,,,!
+    PHASE ORD_BETA %L  2  0.75  0.25   !
+    CONSTITUENT ORD_BETA :A,B,C:A,B,C: !
+    """
+    tdb_str += ordered_phase_str
+
+    dbf = Database(tdb_str)
+
+    comp = {v.X('B'): 0.5}
+    start = 800  # Kelvin
+    step = 5.0
+
+    sol_res = simulate_scheil_solidification(dbf, ['A', 'B'], phases, comp, start, step_temperature=step, stop=1e-8)
     print(f"Converged to stopping criteria: {sol_res.converged}")
 
     phase_amnts = sol_res.phase_amounts